c9149257942a83b2d45297968825cb8d172b4c80
Tech-A08 <tech-a08@Tech-A08s-Air.lan>
2018-03-05 19:06:56 +0000

    first commit

---
+++ person.rb
@@ -0,0 +1,43 @@
+class Person 
+
+    def 
+
+        name = "Deborah"
+            
+        end
+        # create a name variable
+        name = "Steve"
+    end 
+
+    def age
+
+        age = 22
+        # create age variable here
+
+    end
+
+    def children
+        # create children array here
+        ofsprings = ["Perpetual", "Frank", "Solomon", "Bukkie"]
+
+    end
+
+    def address
+
+        # create address hash here
+
+    end
+
+    def password
+
+        # do not change these variables
+        favourite_things = ["motorbike" , "cat" , "travel"]
+        memorable_stuff = {
+          birth_year: 1983,
+          mothers_name: "Eve",
+          birth_town: "Richmond"
+        }
+
+    end
+
+end
